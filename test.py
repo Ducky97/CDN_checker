475d9288f3ef4c8c718c8817fbd668df4b8f4b24
Ducky97 <wangwenya1997@163.com>
2020-09-17 18:20:20 +0800

    modified

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ import queue
 import re
 import os
 import json
+import getopt
 
 
 # 使用Json保存结果
@@ -27,7 +28,7 @@ headers = {
 
 # 获取cname记录
 def get_dns_item(domain_name, crawl_re, item):
-    print(domain_name, crawl_re, crawl_re.url, item)
+    # print(domain_name, crawl_re, crawl_re.url, item)
     checked_dn = domain_name
     if domain_name.count('.') > 1:
         try:
@@ -50,7 +51,7 @@ def get_dns_item(domain_name, crawl_re, item):
             relocated_domain = url.split("/")[2]
             if "?" in relocated_domain:
                 relocated_domain = relocated_domain.split("?")[0]
-            print("!!!!!!!!!!!!!!!!!relocated_domain", relocated_domain)
+            # print("!!!!!!!!!!!!!!!!!relocated_domain", relocated_domain)
             # 看看重定位的网页是不是多级域名
             if relocated_domain.count('.') > 1:
                 # CNAME
@@ -109,7 +110,8 @@ def get_nslookup(domain):
 
 
 def save(domain, result):
-    path = "top-1m"
+    university = domain.split(".")[-3]
+    path = "university/" + university
     with open(path+"/"+domain+".json", "w") as f:
         json.dump(result, f, indent=4, cls=MyEncoder)
     
@@ -124,7 +126,7 @@ def crawl_one_page(domain):
     for protocol in protocol_s:
         url = protocol + "://" + domain
         try:
-            requests_re = requests.get(url, headers = headers)
+            requests_re = requests.get(url, headers = headers, timeout=30)
             # 记录headers
             if protocol not in total_result:
                 total_result[protocol] = dict()
@@ -147,7 +149,7 @@ def crawl_one_page(domain):
                     relocated_domain = requests_re.url.split("/")[2]
                     if "?" in relocated_domain:
                         relocated_domain = relocated_domain.split("?")[0]
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!SSSSSSSSSSSSSSSSSSSS", relocated_domain, domain)
+                    # rint("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!SSSSSSSSSSSSSSSSSSSS", relocated_domain, domain)
                     if relocated_domain != domain:
                         nslookup_result = get_nslookup(relocated_domain)
                         ping_result = get_ping(relocated_domain)
@@ -175,19 +177,75 @@ def crawl_one_page(domain):
       
 
 
+def main(argv):
+    print(argv)
+    # 处理输入
+    # 使用选项 -h help -s --school_name school_name -d -domain --domain_name -t --top-1m
+    # print(sys.argv[1:])
+    try:
+        opts, args = getopt.getopt(argv, "-h -u: -d: -t", ["help", "university=", "domain=", "top"])
+        # print(opts, args)
+    except getopt.GetoptError:
+        print('python3 test.py -h --help -u name1,name2,name3... --university name1,name2,name3... -d name1,name2,name3... --domain name1,name2,name3... -t --top-1m')
+        sys.exit(2)
+    
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print("python3 test.py -h --help -u name1,name2,name3.. --university name1,name2,name3.. -d name1,name2,name3.. --domain name1,name2,name3.. -t --top-1m")
+        elif opt in ("-u", "--university"):
+            university_list = arg.split(',')
+            for university in university_list:
+                path = "dataset/get_fulldomain/output/domain/fulldomain_nd/" + university + ".edu.cn.txt"
+                cmd = "mkdir university/" + university
+                os.system(cmd)
+                domain_set = set()
+                with open(path, 'r') as f:
+                    line = f.readline()
+                    # num = 0
+                    while line: #and num<500:
+                        domain = line.strip()
+                        # print(line.strip(), domain)
+                        # crawl_one_page(domain)
+                        line = f.readline()
+                        domain_set.add(domain)
+                    total_num = len(domain_set)
+                    count = 1
+                    for domain in domain_set:
+                        print(count, "/", total_num, domain)
+                        start = time.clock()
+                        crawl_one_page(domain)
+                        end = time.clock()
+                        # print("use time: ", end-start)
+                        count += 1  
+        elif opt in ("-d", "--domain_name"):
+            domain_name_list = arg.split(",")
+            for domain in domain_name_list:
+                crawl_one_page(domain)
+            print("domain name:", arg)
+        elif opt in ("-t", "--top-1m"):
+            path = "dataset/top-1m.csv"
+            cmd = "mkdir top-1m"
+            os.system(cmd)
+            domain_set = set()
+            with open(path, 'r') as f:
+                line = f.readline()
+                # num = 0
+                while line: #and num<500:
+                    domain = line.strip()
+                    # print(line.strip(), domain)
+                    # crawl_one_page(domain)
+                    line = f.readline()
+                    domain_set.add(domain)
+                total_num = len(domain_set)
+                count = 1
+                for domain in domain_set:
+                    print(count, "/", total_num, domain)
+                    start = time.clock()
+                    # crawl_one_page(domain)
+                    end = time.clock()
+                    # print("use time: ", end-start)
+                    count += 1  
+
+
 if __name__ == "__main__":
-    path = "top-1m.csv"
-    cmd = "mkdir " + path.split('.')[0]
-    os.system(cmd)
-    with open(path, 'r') as f:
-        line = f.readline()
-        num = 0
-        while line and num<500:
-            domain = line.strip().split(',')[1]
-            # print(line.strip(), domain)
-            crawl_one_page(domain)
-            line = f.readline()
-            num += 1
-    # while True:
-    #     domain = input("domain: ")
-    #     crawl_one_page(domain)
+    main(sys.argv[1:])
